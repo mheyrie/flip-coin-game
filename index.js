baa0f4930d3c674fb221c0500f434da1cdcceeba
Mheyrie <omota4fidelity@gmail.com>
2024-02-21 03:08:48 +0100

    Updated using js in-built localStorage and reset button added

--- index.js
+++ index.js
@@ -3,24 +3,46 @@ const coinTail = document.querySelector(".tail").addEventListener("click", secon
 const resultWord = document.querySelector(".result-word")
 const display = document.querySelector(".display-moves")
 const score = document.querySelector(".scores")
+const reset = document.querySelector(".reset-btn").addEventListener("click", resetFunct)
 
-let win = "YOU WIN"
-let loss = "YOU LOSE"
+let showScore = JSON.parse(localStorage.getItem("showScore")) || {
+    wins:0,
+    losses:0
+};
+
+function playGame(guess){
+    const randomNumber = Math.random()
+    const result = (randomNumber<0.5) ? "Head" : "Tail"
+
+    display.textContent = `You: ${guess} || Computer:${result}`
+    if(guess === result){
+        showScore.wins++
+        score.textContent = `Wins: ${showScore.wins} Losses: ${showScore.losses}`
+        resultWord.textContent = "YOU WIN"
+    }else{
+        showScore.losses++
+        score.textContent = `Wins: ${showScore.wins} | Losses: ${showScore.losses}`
+        resultWord.textContent = "YOU LOSE"
+    }
+
+    localStorage.setItem("showScore", JSON.stringify(showScore))
+}
 
 function firstFunct(){
-    const randomNumber = Math.random()
-    const result = (randomNumber>0 && randomNumber<0.5) ? "Head" : "Tail"
-    resultWord.textContent = `You: Head || Computer:${result}`
-    const guess = (result === "Head") ? `${win}` : `${loss}`
-    display.textContent = `${guess}`
-   
+    playGame("Head")
 }
 
 function secondFunct(){
-    const randomNumber = Math.random()
-    const result = (randomNumber>=0.5) ? "Tail" : "Head"
-    resultWord.textContent = `You: Tail || Computer:${result}`
-    const guess = (result === "Tail") ? `${win}` : `${loss}`
-    display.textContent = `${guess}`
+    playGame("Tail")
+}
 
+function resetFunct(){
+    showScore = {
+        wins:0,
+        losses:0
+    };
+    localStorage.setItem("showScore", JSON.stringify(showScore));
+    score.textContent = `Wins: ${showScore.wins} | Losses: ${showScore.losses}`;
+    resultWord.textContent = "";
+    display.textContent = "Click any of the image to begin";
 }
